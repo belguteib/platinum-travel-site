5d7ebe6d984b36125eb72873be06442018c2ed38
B.Belgutei <bb.belgutei@gmail.com>
2020-03-31 16:42:03 +0800

    modal overlay-g buren hiij duuslaa

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -7,8 +7,24 @@ let stickyHeader = new StickyHeader();
 
 new RevealOnScroll(document.querySelectorAll(".feature-item"), 75);
 new RevealOnScroll(document.querySelectorAll(".testimonial"), 50);
-
 let mobileMenu = new MobileMenu();
+let modal;
+
+document.querySelectorAll(".open-modal").forEach(el => {
+  el.addEventListener("click", e => {
+    e.preventDefault();
+    if (typeof modal == "undefined") {
+      import(/*webpackChunkName: "modal" */ "./modules/Modal")
+        .then(x => {
+          modal = new x.default();
+          setTimeout(() => modal.openModal(), 20);
+        })
+        .catch(() => console.log("Файлыг дуудахад алдаа гарлаа"));
+    } else {
+      modal.openModal();
+    }
+  });
+});
 
 if (module.hot) {
   module.hot.accept();
