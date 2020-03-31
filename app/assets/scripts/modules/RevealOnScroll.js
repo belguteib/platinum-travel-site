ea20e942d690d0d23ed936c464a95302b8be42ac
B.Belgutei <bb.belgutei@gmail.com>
2020-03-31 12:38:01 +0800

    reaveal feature-iig buren hiij duuslaa

---
+++ app/assets/scripts/modules/RevealOnScroll.js
@@ -0,0 +1,59 @@
+import throttle from "lodash/throttle";
+import debouce from "lodash/debounce";
+
+class RevealOnScroll {
+  constructor(els, revealPoint) {
+    this.revealPoint = revealPoint;
+    this.itemsToReveal = els;
+    this.browserHeight = window.innerHeight;
+    this.hideInitially();
+    this.scrollThrottle = throttle(this.calcCaller, 200).bind(this);
+    this.events();
+  }
+
+  events() {
+    window.addEventListener("scroll", this.scrollThrottle);
+    window.addEventListener(
+      "resize",
+      debouce(() => {
+        console.log("hiigdlee");
+        this.browserHeight = window.innerHeight;
+      }, 300)
+    );
+  }
+
+  calcCaller() {
+    console.log("scroll function working");
+    this.itemsToReveal.forEach(el => {
+      if (el.isRevealed == false) {
+        this.calculateIfScrolledTo(el);
+      }
+    });
+  }
+
+  calculateIfScrolledTo(el) {
+    if (window.scrollY + this.browserHeight > el.offsetTop) {
+      console.log("totso");
+      let scrollPercent =
+        (el.getBoundingClientRect().y / this.browserHeight) * 100;
+      if (scrollPercent < this.revealPoint) {
+        el.classList.add("reveal-item--is-visible");
+        el.isRevealed = true;
+        if (el.isLastItem == true) {
+          window.removeEventListener("scroll", this.scrollThrottle);
+        }
+      }
+    }
+  }
+
+  hideInitially() {
+    this.itemsToReveal.forEach(el => {
+      el.classList.add("reveal-item");
+      el.isRevealed = false;
+    });
+
+    this.itemsToReveal[this.itemsToReveal.length - 1].isLastItem = true;
+  }
+}
+
+export default RevealOnScroll;
